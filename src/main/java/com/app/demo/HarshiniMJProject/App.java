911ac3b114c02942c73db79ca547c0932fde5818
bhuvan <bhuvanmaddela@gmail.com>
2023-12-13 15:54:38 +0530

    Demo for users

---
+++ src/main/java/com/app/demo/HarshiniMJProject/App.java
@@ -0,0 +1,13 @@
+package com.app.demo.HarshiniMJProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello from Harshini" );
+    }
+}
